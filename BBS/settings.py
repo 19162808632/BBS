1d1f1a2e678463f3d400d3a5f3be57f9c23b3868
zhangdingwen <1106886337@qq.com>
2021-04-20 20:49:13 +0800

    完善代码逻辑

--- BBS/settings.py
+++ BBS/settings.py
@@ -64,14 +64,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
+AUTH_PASSWORD_VALIDATORS = [{
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
+    },{
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
+    },{
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
@@ -105,4 +102,4 @@ AUTH_USER_MODEL = 'app01.UserInfo'
 LOGIN_URL = '/login/'
 
 # 配置用户上传的文件存储位置
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 文件名 随你 自己
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
